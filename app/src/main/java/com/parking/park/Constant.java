20da61a7740b64754bf3d0fd65fe67ddab1c7e9c
gaolong <klgaolong@qq.com>
2019-03-27 12:27:16 +0800

    端口9999

--- app/src/main/java/com/parking/park/Constant.java
+++ app/src/main/java/com/parking/park/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
     public static String LOCAL_CONNECTION_IP = "";
 
 
-    public static int PORT = 8080;
+    public static int PORT = 9999;
 
     public static final long AD_LOOP_INTERVAL = 3000l;
     public static final long AD_SHOW_DELAY= 3000l;
