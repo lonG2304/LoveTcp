c311cfa987a1d5d828b79bd4e0602011b732c3ee
gaolong <klgaolong@qq.com>
2019-03-30 17:02:59 +0800

    1

--- app/src/main/java/com/parking/park/tcp/ParkingChannel.java
+++ app/src/main/java/com/parking/park/tcp/ParkingChannel.java
@@ -1,8 +1,11 @@
 package com.parking.park.tcp;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
@@ -20,6 +23,9 @@ public class ParkingChannel extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
+
+        ByteBuf delimiter=Unpooled.copiedBuffer("\n".getBytes());
+        ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
         pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
         pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
         pipeline.addLast(new ParkingHandler(listener));
